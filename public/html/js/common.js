e0ac7994e4b17985a0c5238bf082f949329f69c2
weibaoyuan <wbyuan1989@163.com>
2013-12-31 11:05:08 +0800

    恢复更改

---
+++ public/html/js/common.js
@@ -0,0 +1,99 @@
+/**
+ * Created by ui-1 on 13-12-23.
+ */
+$(function(){
+    $(".sub-nav ul li a").click(function(){
+        $(".sub-nav ul li a").removeClass("active");
+        $(this).addClass("active");
+        $(".product-side>div").addClass("d-n");
+        $(".product-side>div").eq($(".sub-nav ul li a").index($(this))).removeClass("d-n");
+    });
+});
+/*用户中心折叠效果*/
+$(function(){
+    $(".dropdown>a").click(function(){
+        var menu=$(this).next("ul");
+        if(menu.css("display")=="none")
+        {
+            menu.css({"display":"block"});
+            $(this).parent("li").find("span").addClass("icon-arrow-down");
+        }
+        else{
+            menu.css({"display":"none"});
+            $(this).parent("li").find("span").removeClass("icon-arrow-down");
+        }
+    });
+});
+/*弹出登录框*/
+$(function(){
+    var screenwidth,screenheight,mytop,getPosLeft,getPosTop;
+     screenwidth = $(window).width();
+     screenheight = $(window).height();
+     mytop = $(document).scrollTop();
+     getPosLeft = screenwidth/2 - 260;
+     getPosTop = screenheight/2 - 150;
+     $(".login2").css({"left":getPosLeft,"top":getPosTop});
+     $(window).resize(function(){
+        screenwidth = $(window).width();
+        screenheight = $(window).height();
+         mytop = $(document).scrollTop();
+        getPosLeft = screenwidth/2 - 260;
+        getPosTop = screenheight/2 - 150;
+        $(".login2").css({"left":getPosLeft,"top":getPosTop+mytop});
+     });
+    $(window).scroll(function(){
+        screenwidth = $(window).width();
+        screenheight = $(window).height();
+        mytop = $(document).scrollTop;
+        getPosLeft = screenwidth/2 - 260;
+        getPosTop = screenheight/2 - 150;
+        $(".login2").css({"left":getPosLeft,"top":getPosTop+mytop});
+    });
+    $("#popup").click(function(){
+        $(".login2").fadeIn("fast");
+        $("body").append("<div id='greybackground'></div>");
+        var documentheight = $(document).height();
+        $("#greybackground").css({"opacity":"0.5","height":documentheight});
+        return false;
+    });
+    $("#close").click(function() {
+        $(".login2").hide();
+        $("#greybackground").remove();
+        return false;
+    });
+});
+
+/*今日热门图片切换*/
+$(function(){
+    var page=1;
+    var i=5;
+    var content=$(".img-show");
+    var content_list=$(".img-show ul");;
+    var v_width=content.width();
+    var len=content_list.length;
+    var page_cont=Math.ceil(len/i);
+    $(".next").click(function(){
+        if(!content_list.is(":animated")){
+            if(page==page_cont){
+                content_list.animate({left:'0px'},'show');
+                page=1;
+            }
+            else{
+                content_list.animate({left:'-='+v_width},'show');
+                page++;
+            }
+        }
+    });
+    $(".prev").click(function(){
+        if(!content_list.is(":animated")){
+            if(page==1){
+                content_list.animate({left:'-='+v_width*(page_cont-1)},'show');
+                page=1;
+            }
+            else{
+                content_list.animate({left:'+='+v_width},'show');
+                page--;
+            }
+        }
+    });
+});
