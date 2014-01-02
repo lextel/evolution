75bc016e6608bc2e45d2a5274b27d2af3492de3b
lan <lan@lan.(none)>
2014-01-02 23:03:18 +0800

    后台订单列表 中奖列表细节

--- fuel/app/views/member/sms/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Editing <span class='muted'>Member_sm</span></h2>
-<br>
-
-<?php echo render('member/sms/_form'); ?>
-<p>
-	<?php echo Html::anchor('member/sms/view/'.$member_sm->id, 'View'); ?> |
-	<?php echo Html::anchor('member/sms', 'Back'); ?></p>
