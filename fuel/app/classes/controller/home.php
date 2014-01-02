74b7cee3a975c1663aedac714e438156198854fa
lan <lan@lan.(none)>
2014-01-02 15:40:31 +0800

    用户TA的主页

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -45,7 +45,15 @@ class Controller_Home extends Controller_Frontend {
     */
     public function action_orders($member_id){
         $member = Model_Member::find($member_id);
-        $wins = Model_Lottery::find_by_member_id($member_id);
+        $count = Model_Order::count(['where'=>['member_id'=>$member_id]]);
+        $page = new \Helper\Page();
+        $config = $page->setCofigPage('u/order/p', $count, 4, 4);
+        $pagination = Pagination::forge('horders', $config);
+        $wins = Model_Order::find('all',
+                        ['where'=>['member_id'=>$member_id]],
+                        'rows_limit'=>$pagination->per_page,
+                        'rows_offset'=>$pagination->offset,]
+                        );
         $view = View::forge('index/home_orders');
         $view->set('wins', $wins);
         $view->set('member', $member);
