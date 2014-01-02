01c774aa4c759273a9628c2096097fbdad0e82c1
weibaoyuan <wbyuan1989@163.com>
2014-01-02 15:53:15 +0800

    修改用户中心css

--- fuel/app/views/posts/view.php
+++ fuel/app/views/posts/view.php
@@ -42,7 +42,7 @@
        </div>
        <div class="btn-group">
            <?php echo Html::anchor('javascript:;', '喜欢(<s>'.$post->up.'</s>)', array('class'=>'btn btn-link btn-up', 'id'=>$post->id));?>
-           <?php echo Html::anchor('javascript:;', '评论(<s>'.$post->comment_count.'</s>)', array('class'=>'btn btn-link btn-comment'));?>
+           <span>评论(<s><?php echo $post->comment_count;?></s>)</span>
        </div>
    </div>
     <div class="content-right fr">
