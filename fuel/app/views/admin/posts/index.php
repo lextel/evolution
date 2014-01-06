6b3b264514edb6a126c90714f8ae81a5843d60af
lan <lan@lan.(none)>
2014-01-06 21:11:51 +0800

    后台晒单管理修改

--- fuel/app/views/admin/posts/index.php
+++ fuel/app/views/admin/posts/index.php
@@ -14,7 +14,7 @@ $(function(){
     '2' => '审核不通过晒单列表',
     '3' => '已删除晒单列表',
     ],
-    ['class'=>'postactive']
+    ['class'=>'postactive', 'style'=>'height:34px']
 );?>
 <br>
 <?php if ($posts): ?>
@@ -24,24 +24,20 @@ $(function(){
             <th>标题</th>
             <th>标题</th>
             <th>内容</th>
-            <th>审核状态</th>
-            <th>商品名</th>
+            <th>状态</th>
             <th>发布人</th>
-            <th>商品分类</th>
-            <th>期数</th>
+            <th>商品名称</th>
             <th></th>
         </tr>
     </thead>
     <tbody>
 <?php foreach ($posts as $item){ ?>     <tr>
             <th><?php echo $item->id; ?></th>
-            <td><?php echo $item->title; ?></td>
-            <td><?php echo mb_substr($item->desc, 0, 42,'utf-8');?></td>
-            <td><?php echo $item->status; ?></td>
-            <td><?php echo $item->item_id; ?></td>
-            <td><?php echo $item->member_id; ?></td>
-            <td><?php echo $item->type_id; ?></td>
-            <td>第<?php echo $item->phase_id; ?>期</td>
+            <td><?php echo mb_substr($item->title, 0, 16,'utf-8'); ?>...</td>
+            <td><?php echo mb_substr($item->desc, 0, 16,'utf-8');?>...</td>
+            <td><?php echo $getStatus($item); ?></td>
+            <td><?php echo $getUser($item->member_id)->nickname; ?></td>
+            <td>第<?php echo $getPhase($item)->phase_id; ?>期 <?php echo mb_substr($getPhase($item)->title, 0, 16,'utf-8'); ?>...</td>
             <td>
                 <?php echo Html::anchor('admin/posts/view/'.$item->id, '审核'); ?> |
                 <?php echo Html::anchor('javascript:;', '删除', array('onclick' => "return confirm('你确定需要删除吗?')")); ?>
