c8d36ca2b09600a0753e2c7653557216efd03523
lan <lan@lan.(none)>
2014-01-08 18:30:16 +0800

    TA的主页调整

--- fuel/app/views/index/home_posts.php
+++ fuel/app/views/index/home_posts.php
@@ -3,7 +3,7 @@
             <li><?php echo Html::anchor('u/'.$member->id, '主页');?></li>
             <li><?php echo Html::anchor('u/'.$member->id.'/orders', '乐拍记录');?></li>
             <li><?php echo Html::anchor('u/'.$member->id.'/wins', '获得的商品');?></li>
-            <li><?php echo Html::anchor('u/'.$member->id.'/posts', '晒单', ['class'=>'active']);?></li>
+            <li class="active"><?php echo Html::anchor('u/'.$member->id.'/posts', '晒单');?></li>
         </ul>
         <!--晒单-->
         <div class="home-c">
