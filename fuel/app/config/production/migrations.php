8e10438dc1860f913a22d6cfa3b0bbfe6b9ee683
weelion <weelion@qq.com>
2014-01-08 16:14:00 +0800

    fixes #27 fixex #28

---
+++ fuel/app/config/production/migrations.php
@@ -0,0 +1,39 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 
+			array(
+				0 => '005_create_members',
+				1 => '013_create_comments',
+				2 => '027_create_cates',
+				3 => '031_create_logs',
+				4 => '032_create_member_moneylogs',
+				5 => '034_create_member_addresses',
+				6 => '041_create_phases',
+				7 => '042_create_items',
+				8 => '046_create_notices',
+				9 => '047_create_orders',
+				10 => '048_create_posts',
+				11 => '049_create_ads',
+				12 => '050_create_member_sms',
+			),
+		),
+		'module' => 
+		array(
+		),
+		'package' => 
+		array(
+			'auth' => 
+			array(
+				0 => '001_auth_create_usertables',
+				1 => '002_auth_create_grouptables',
+				2 => '003_auth_create_roletables',
+			),
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
