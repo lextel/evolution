4bfd5f8489e3e3dd569806d7f351b9df8cd9d95a
xj <413146620@qq.com>
2014-01-08 13:04:31 +0800

    数据库部署开发机

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -1,48 +1,33 @@
 <?php
 return array(
-	'version' =>
+	'version' => 
 	array(
-		'app' =>
+		'app' => 
 		array(
-			'default' =>
+			'default' => 
 			array(
 				0 => '001_create_users',
-				1 => '002_create_items',
-				2 => '003_create_phases',
-				3 => '005_create_members',
-				4 => '008_create_orders',
-				5 => '009_create_member_infos',
-				6 => '010_create_lotteries',
-				7 => '011_create_member_addresses',
-				8 => '012_create_posts',
-				9 => '013_create_comments',
-				10 => '014_create_member_addresses',
-				11 => '027_create_cates',
-				12 => '031_create_logs',
-				13 => '032_create_member_moneylogs',
-				14 => '032_create_phases',
-				15 => '033_create_member_addresses',
-				16 => '034_create_member_addresses',
-				17 => '040_create_posts',
-				18 => '041_create_phases',
-				19 => '042_create_items',
-				20 => '043_create_notices',
-				21 => '044_create_orders',
-				22 => '044_create_posts',
-				23 => '045_create_lotteries',
-				24 => '046_create_notices',
-				25 => '047_create_orders',
-				26 => '048_create_posts',
-				27 => '049_create_ads',
-				28 => '050_create_member_sms',
+				1 => '005_create_members',
+				2 => '013_create_comments',
+				3 => '027_create_cates',
+				4 => '031_create_logs',
+				5 => '032_create_member_moneylogs',
+				6 => '034_create_member_addresses',
+				7 => '041_create_phases',
+				8 => '042_create_items',
+				9 => '046_create_notices',
+				10 => '047_create_orders',
+				11 => '048_create_posts',
+				12 => '049_create_ads',
+				13 => '050_create_member_sms',
 			),
 		),
-		'module' =>
+		'module' => 
 		array(
 		),
-		'package' =>
+		'package' => 
 		array(
-			'auth' =>
+			'auth' => 
 			array(
 				0 => '001_auth_create_usertables',
 				1 => '002_auth_create_grouptables',
