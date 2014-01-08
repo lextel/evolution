fc5757e625d7f8ccb459ed2c8e2d3f3fd5cc8bfa
weelion <weelion@qq.com>
2014-01-08 16:35:50 +0800

    分类导航切换交互 fixes #66

--- public/assets/js/item/index.js
+++ public/assets/js/item/index.js
@@ -2,6 +2,8 @@ $(function(){
     $('.cateNav').hover(function() {
         var idx = $(this).index();
         idx = idx-1;
+        $('.cateNav').removeClass('active');
+        $(this).addClass('active');
         var dom = $(this).parent().next();
         dom.find('dl').hide();
         dom.find('dl').eq(idx).show();
