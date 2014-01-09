45da05b27852bbab12b74c33368fe7c01ebcf292
weelion <weelion@qq.com>
2014-01-09 18:23:41 +0800

    修复member sms字段

--- fuel/app/classes/model/member/sm.php
+++ fuel/app/classes/model/member/sm.php
@@ -5,7 +5,7 @@ class Model_Member_Sm extends Model
 {
 	protected static $_properties = array(
 		'id',
-		'ower_id',
+		'owner_id',
 		'title',
 		'type',
 		'user_id',
@@ -28,7 +28,7 @@ class Model_Member_Sm extends Model
 	public static function validate($factory)
 	{
 		$val = Validation::forge($factory);
-		$val->add_field('ower_id', 'Ower Id', 'required|valid_string[numeric]');
+		$val->add_field('owner_id', 'Owner Id', 'required|valid_string[numeric]');
 		$val->add_field('title', 'Title', 'required|max_length[255]');
 		$val->add_field('type', 'Type', 'required|max_length[255]');
 		$val->add_field('user_id', 'User Id', 'required|valid_string[numeric]');
