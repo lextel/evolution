4214d562f34116c50583596234ef2dc9bbf0f879
weibaoyuan <wbyuan1989@163.com>
2014-01-10 14:03:06 +0800

    修改用户中心css

--- fuel/app/views/index/home_posts.php
+++ fuel/app/views/index/home_posts.php
@@ -5,33 +5,32 @@
             <li><?php echo Html::anchor('u/'.$member->id.'/wins', '获得的商品');?></li>
             <li class="active"><?php echo Html::anchor('u/'.$member->id.'/posts', '晒单');?></li>
         </ul>
-        <!--晒单-->
-        <div class="home-c">
+</div>
+ <!--晒单-->
+        <div class="bask-menu">
             <?php if($posts) { ?>
-            <dl class="bask-menu">
+            <dl>
                 <?php foreach($posts as $item) { ?>
                 <dd>
-                    <div class="img-box fl">
+                    <div class="img-box img-md fl">
                         <?php echo Html::anchor('/p/'.$item->id, Html::img('/'.$item->topimage));?>
                     </div>
-                    <div class="info-side fr">
-                        <h4 class="fl"><?php echo Html::anchor('/p/'.$item->id, $item->title);?>
-                            <small> <?php echo \Helper\Timer::friendlyDate($item->created_at);?></small>
-                        </h4>
+                    <div class="info-side fl">
+                        <h5><?php echo Html::anchor('/p/'.$item->id, $item->title);?> </h5>
+                        <div class"datetime"> <?php echo \Helper\Timer::friendlyDate($item->created_at);?></div>
                         <div class="text-content">
                             <?php echo $item->desc ;?>
                         </div>
-                        <div class="btn-group">
-                            <a href="javascript:;" class="btn btn-link">喜欢<s>(<?php echo $item->up ;?>)</s></a>
-                            <a href="javascript:;" class="btn btn-link">评论<s>(<?php echo $item->comment_count ;?>)</s></a>
+                        <div class="btn-group sns-bar">
+                            <a href="javascript:;" class="btn-link sns-love">喜欢<s>(<?php echo $item->up ;?>)</s></a>
+                            <a href="javascript:;" class="btn-link sns-comment">评论<s>(<?php echo $item->comment_count ;?>)</s></a>
                         </div>
                     </div>
                 </dd>
                 <?php } ?>
             </dl>
-            <?php echo Pagination::instance('hposts')->render();?> 
+            <?php echo Pagination::instance('hposts')->render();?>
             <?php } else { ?>
             <p> 该用户暂时没任何的晒单记录</p>
             <?php } ?>
         </div>
-</div>
--- fuel/app/views/index/home_wins.php
+++ fuel/app/views/index/home_wins.php
@@ -5,8 +5,8 @@
                 <li class="active"><?php echo Html::anchor('u/'.$member->id.'/wins', '获得的商品');?></li>
                 <li><?php echo Html::anchor('u/'.$member->id.'/posts', '晒单');?></li>
             </ul>
-            
-              <!--获得的商品-->
+        </div>
+ <!--获得的商品-->
             <div class="home-c">
                 <?php if($wins) { ?>
                 <dl>
@@ -16,18 +16,17 @@
                             <?php echo Html::anchor('m/'.$item->phase_id, Html::img($getItemInfo($item->item_id)->image));?>
                         </div>
                         <div class="title-box">
-                            <h4><?php echo Html::anchor('m/'.$item->phase_id, $getItemInfo($item->item_id)->title);?></h4>
+                            <h3 class="title-sm"><?php echo Html::anchor('m/'.$item->phase_id, $getItemInfo($item->item_id)->title);?></h3>
                             <span class="price">价值 <b>￥<?php echo $getItemInfo($item->item_id)->price;?>.00</b></span>
                             <div class="number">幸运乐拍码：<b class="y"><?php echo $item->code;?></b></div>
                             <div class="datetime">揭晓时间：<?php echo \Helper\Timer::friendlyDate($getPhaseInfo($item->phase_id)->opentime);?></div>
                         </div>
                     </dd>
                     <?php } ?>
-                </dl> 
-                <br />  
-                 <?php echo Pagination::instance('hwins')->render();?>  
+                </dl>
+                <br />
+                 <?php echo Pagination::instance('hwins')->render();?>
                  <?php } else { ?>
                  <p> 该用户暂时没任何的中奖记录</p>
                  <?php } ?>
             </div>
-        </div>
