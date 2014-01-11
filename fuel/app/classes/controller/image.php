695fb7651b039b2eba8227775972538df4d92daa
weelion <weelion@qq.com>
2014-01-11 12:53:18 +0800

    缩略图bug fixes #74

--- fuel/app/classes/controller/image.php
+++ fuel/app/classes/controller/image.php
@@ -7,8 +7,13 @@ class Controller_Image extends Controller_Template {
 
     public function action_index() {
 
+        Config::load('upload');
         $size = $this->param('size');
+        $uploadConfig = Config::get('item');
         $link = $this->param('link') . '.jpg';
+        if(!file_exists($uploadConfig['path'].$link)) {
+            $link = $this->param('link') . '.jpeg';
+        }
 
         $image = new \Classes\Image();
         $link = $image->resize($link, $size);
