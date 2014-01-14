43392aa84cf7b42efffbcaac737378392ee1b514
weelion <weelion@qq.com>
2014-01-14 10:12:39 +0800

    后台登陆页面中文化

--- fuel/app/views/admin/login.php
+++ fuel/app/views/admin/login.php
@@ -11,25 +11,25 @@
             <?php endif; ?>
 
             <div class="form-group <?php echo ! $val->error('email') ?: 'has-error' ?>">
-                <label for="email">Email or Username:</label>
-                <?php echo Form::input('email', Input::post('email'), array('class' => 'form-control', 'placeholder' => 'Email or Username', 'autofocus')); ?>
+                <label for="email">账号:</label>
+                <?php echo Form::input('email', Input::post('email'), array('class' => 'form-control', 'placeholder' => '用户名或邮箱', 'autofocus')); ?>
 
                 <?php if ($val->error('email')): ?>
-                    <span class="control-label"><?php echo $val->error('email')->get_message('You must provide a username or email'); ?></sőan>
+                    <span class="control-label"><?php echo $val->error('email')->get_message('请输入账号'); ?></sőan>
                 <?php endif; ?>
             </div>
 
             <div class="form-group <?php echo ! $val->error('password') ?: 'has-error' ?>">
-                <label for="password">Password:</label>
-                <?php echo Form::password('password', null, array('class' => 'form-control', 'placeholder' => 'Password')); ?>
+                <label for="password">密码:</label>
+                <?php echo Form::password('password', null, array('class' => 'form-control', 'placeholder' => '密码')); ?>
 
                 <?php if ($val->error('password')): ?>
-                    <span class="control-label"><?php echo $val->error('password')->get_message(':label cannot be blank'); ?></span>
+                    <span class="control-label"><?php echo $val->error('password')->get_message('请输入密码'); ?></span>
                 <?php endif; ?>
             </div>
 
             <div class="actions">
-                <?php echo Form::submit(array('value'=>'Login', 'name'=>'submit', 'class' => 'btn btn-lg btn-primary btn-block')); ?>
+                <?php echo Form::submit(array('value'=>'登陆', 'name'=>'submit', 'class' => 'btn btn-lg btn-primary btn-block')); ?>
             </div>
 
         <?php echo Form::close(); ?>
