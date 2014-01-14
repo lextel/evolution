b8c4c05fac00b5c46d686051418fd7dae43bd916
weelion <weelion@qq.com>
2014-01-14 16:06:34 +0800

    添加收藏功能

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -676,6 +676,26 @@ $(function(){
       });
 });
 
+// 收藏
+function addFavorite(sURL, sTitle)
+{
+    try
+    {
+        window.external.addFavorite(sURL, sTitle);
+    }
+    catch (e)
+    {
+        try
+        {
+            window.sidebar.addPanel(sTitle, sURL, "");
+        }
+        catch (e)
+        {
+            alert("加入收藏失败，请使用Ctrl+D进行添加");
+        }
+    }
+}
+
 
 
 
