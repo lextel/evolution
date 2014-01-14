7b0d16ecfe094e2030dfec013304b996349a5a45
lan <lan@lan.(none)>
2014-01-14 17:50:10 +0800

    修改登陆 注册返回提示

--- fuel/app/views/member/signup.php
+++ fuel/app/views/member/signup.php
@@ -1,47 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>用户注册</title>
-    <?php echo Asset::css('common.css');?>
+
     <?php echo Asset::css('style.css');?>
-    <?php echo Asset::js('jquery.min.js');?>
     <?php echo Asset::css('member/validfrom_style.css'); ?>
     <?php echo Asset::js('Validform_v5.3.2_min.js'); ?>
-
-</head>
-<body>
     <div class="register w">
         <div class="title">
             <h3 class="fl">新用户注册</h3>
             <ul class="fl">
-                <li><a href="">1填写注册信息</a></li>
-                <li><a href="">2填写注册信息</a></li>
-                <li><a href=""></a></li>
+                <li><a href="javascript:;">1填写注册信息</a></li>
+                <li><a href="javascript:;">>>&nbsp;&nbsp;2填写注册信息</a></li>
+                <li><a href="javascript:;"></a></li>
             </ul>
-            <div class="login fr">
-                已经是会员，直接登录
-                <?php echo Html::anchor('signin', '登录', array('class' => ''));?>
+            <div class="login fr">               
+                <?php echo Html::anchor('signin', '已经是会员，直接登录', array('class' => 'blue'));?>
             </div>
         </div>
-        <?php if($signup_error) { ?>
-            <span><?php echo $signup_error; ?></span>
-        <?php } ?>
+
         <?php echo Form::open(array("class"=>"register-form demoform")); ?>
             <ul>
                 <li>
                    <?php echo Form::label('用户邮箱'); ?>
-                   <?php echo Form::input('username', '', array('type'=>"text",'datatype'=>'e','name'=>'username','errorms'=>'邮箱帐号格式不正确','nullmsg'=>'请输入邮箱帐号')); ?>
+                   <?php echo Form::input('username', Session::get_flash('username', ''), array('type'=>"text",'datatype'=>'e',
+                         'name'=>'username','errorms'=>'邮箱帐号格式不正确','nullmsg'=>'请输入邮箱帐号')); ?>
+                   <?php if (Session::get_flash('usernameError', null)) { ?>
+                   <span class="Validform_checktip Validform_wrong"><?php echo Session::get_flash('usernameError');?></span>
+                   <?php }else{?>
                    <span class="Validform_checktip"></span>
+                   <?php } ?>
                 </li>
                 <li>
                    <?php echo Form::label('输入密码'); ?>
-                   <?php echo Form::input('password', '', array('type'=>"password",'class' => 'inputxt Validform_error', 'name'=>'userpassword','datatype'=>'*6-18','errorms'=>'请输入6-18位密码','nullmsg'=>'请输入6-18位密码')); ?>
+                   <?php echo Form::input('password', '', ['type'=>"password",'class' => 'inputxt Validform_error', 
+                       'name'=>'userpassword','datatype'=>'*6-18','errorms'=>'请输入6-18位密码','nullmsg'=>'请输入6-18位密码']); ?>
                    <span class="Validform_checktip"></span>
                 </li>
                 <li>
                    <?php echo Form::label('确认密码'); ?>
-                   <input type="password" value="" name="userpassword2" class="inputxt" datatype="*6-20" recheck="password">
+                   <input type="password" value="" name="userpassword2" class="inputxt" datatype="*6-18" recheck="password">
                    <span class="Validform_checktip"></span>
                 </li>
                 <li><!--<a href="" class="btn btn-red fl">同意协议并注册</a>-->
@@ -58,7 +52,5 @@ $(function(){
 	$(".demoform").Validform({
 	tiptype:4,
 	});
-});
+});  
 </script>
-</body>
-</html>
