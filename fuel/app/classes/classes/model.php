fcaba8a04d27f825a84b7573a1b9f3068a5d7ff4
weelion <weelion@qq.com>
2014-01-17 15:08:35 +0800

    model通用方法

--- fuel/app/classes/classes/model.php
+++ fuel/app/classes/classes/model.php
@@ -25,6 +25,10 @@ class Model extends \Orm\Model {
     public static function getIds($modelObj, $idFields) {
 
         $data = [];
+        $count = count($idFields);
+        for($i = 0; $i < $count; $i++) {
+            $data[$i] = 0;
+        }
         foreach($modelObj as $item) {
             foreach($idFields as $key => $field) {
                 $data[$key][] = $item->$field;
