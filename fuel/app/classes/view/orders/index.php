1ce9c4fbe2fb019f3f60bc0f93fcf0e06e00a697
lan <lan@lan.(none)>
2014-01-23 15:31:54 +0800

    增加历史订单列表

--- fuel/app/classes/view/orders/index.php
+++ fuel/app/classes/view/orders/index.php
@@ -2,8 +2,12 @@
 
 class View_Orders_index extends Viewmodel
 {
-	public function view()
-	{
-		$this->content = "Orders &raquo; index";
-	}
+    public function view()
+    {
+
+    }
+
+    public function set_view(){
+        $this->_view = View::forge('orders/index');
+   }
 }
