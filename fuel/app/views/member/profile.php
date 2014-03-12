83b96669a18e3df518924c4d5595caedab9dac7a
wbyuan <wbuan1989@163.com>
2014-03-12 14:28:47 +0800

    修改用户中心

--- fuel/app/views/member/profile.php
+++ fuel/app/views/member/profile.php
@@ -36,7 +36,7 @@ $(function(){
                 <?php echo Form::input('username', Input::post('username', $member->email), array('type'=>"text",'name'=>'username',
                 'datatype'=>'e','errorms'=>'请输入邮箱帐号', 'style'=>'border: 0;', 'readonly')); ?>
                 <?php if (!Model_Member_Email::check_emailok($member->email)) {  ?>
-                <a href="javascript:;" class="btn btn-red btn-sx  btn-checkemail" style="margin-left: 0px;margin-top: -5px;">验证</a>
+                <a href="javascript:;" class="btn btn-red btn-checkemail" style="margin-left: 0px;margin-top: -5px;">验证</a>
                 <?php }else{ ?>
                 <a href="javascript:;" class="btn  btn-email" style="margin-left: 0px;margin-top: -5px;">已验证</a>
                 <?php }?>
@@ -57,7 +57,7 @@ $(function(){
                 <span class="Validform_checktip"></span>
             </li>
             <li>
-                <button class="btn btn-red " type="submit" >提交</button>
+                <button class="btn btn-red btn-sx" type="submit" >提交</button>
             </li>
             <?php echo Form::close(); ?>
         </ul>
