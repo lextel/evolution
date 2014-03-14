d21c0cce5107a3a83bc5c67757687eed4089af7f
weelion <weelion@qq.com>
2014-03-14 13:43:23 +0800

    马甲乐拍遗漏危机

---
+++ public/assets/js/admin/ghost/sell.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('.ghostOrder').click(function() {
+        var id = $(this).attr('data-id');
+        var num = $('.orderNum'+id).val();
+        var mid = $('.orderMid'+id).val();
+        $.ajax({
+            url: OPERATE_URL + '/' + id + '/' + num + '/' + mid,
+            type: 'get',
+            dataType: 'json',
+            success: function(data) {
+                if(data.code == 0) {
+                    $('#join'+id).html('<span style="color: green">' + data.data.remain + '</span>').delay(1000).html(data.data.joined);
+                }
+
+                alert(data.msg);
+            },
+            error: function() {
+                alert('请求失败');
+            }
+        });
+    });
+});
