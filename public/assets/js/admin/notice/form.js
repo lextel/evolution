2bea607f92322aab78a374ebbaf478a665191fac
weelion <weelion@qq.com>
2014-04-01 20:18:15 +0800

    公告js

---
+++ public/assets/js/admin/notice/form.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    // 实例化编辑器
+    var ue = new UE.ui.Editor();
+    ue.render("form_desc");
+
+    // 预览功能修改                       
+    $(document).on('click', '#edui173', function() {
+        $('.navbar-fixed-top').hide();
+    });
+
+    // 关闭预览
+    $(document).on('click', '#edui172', function() {
+        $('.navbar-fixed-top').show();
+    });
+
+    console.log(URL);
+
+});
