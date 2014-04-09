182c24d920467175c1c4cfbede385afa06dcafec
weelion <weelion@qq.com>
2014-04-09 15:47:14 +0800

    游戏专题临时上传

---
+++ fuel/app/views/special/game.php
@@ -0,0 +1,96 @@
+<?php echo Asset::css(['style.css']);?>
+<style>
+.w {
+    width: 100%;
+    overflow: visible;
+}
+.navbar {
+    margin-bottom: 0;
+}
+.column-skin dd {
+    margin-right: 37px;
+}
+.footer-help  {
+    width: 980px !important;
+    overflow: hidden !important;
+}
+</style>
+<div class="game-hd"></div>
+<div class="game-bd">
+    <div class="game-column">
+        <ul>
+            <li>
+                <dl class="game-count">
+                    <dt>00</dt>
+                    <dd>时</dd>
+                    <dt>00</dt>
+                    <dd>分</dd>
+                    <dt>00</dt>
+                    <dd>秒</dd>
+                </dl>
+                <div class="imgBox">
+                    <a href="http://www.lltao.com/m/1"><img src="../assets/img/game01.png" alt=""/></a>
+                </div>
+                <div class="tit">Sony PlayStation4 索尼PS4次世代游戏主机</div>
+                <div class="fd-col">
+                    <span class="money">参考价：3699.00元</span>
+                    <a href="http://www.lltao.com/m/1" class="lol-btn-sm"></a>
+                </div>
+            </li>
+            <li>
+                <dl class="game-count">
+                    <dt>00</dt>
+                    <dd>时</dd>
+                    <dt>00</dt>
+                    <dd>分</dd>
+                    <dt>00</dt>
+                    <dd>秒</dd>
+                </dl>
+                <div class="imgBox">
+                    <a href="http://www.lltao.com/m/71"> <img src="../assets/img/game02.png" alt=""/></a>
+                </div>
+                <div class="tit">Sony Playstation Vita 索尼PSV 掌上游戏机</div>
+                <div class="fd-col">
+                    <span class="money">参考价：1599.00元</span>
+                    <a href="http://www.lltao.com/m/71" class="lol-btn-sm"></a>
+                </div>
+            </li>
+            <li>
+                <dl class="game-count">
+                    <dt>00</dt>
+                    <dd>时</dd>
+                    <dt>00</dt>
+                    <dd>分</dd>
+                    <dt>00</dt>
+                    <dd>秒</dd>
+                </dl>
+                <div class="imgBox">
+                    <a href="http://www.lltao.com/m/3"><img src="../assets/img/game03.png" alt=""/></a>
+                </div>
+                <div class="tit">Microsoft 微软Xbox one多媒体游戏主机</div>
+                <div class="fd-col">
+                    <span class="money">参考价：3799.00元</span>
+                    <a href="http://www.lltao.com/m/3" class="lol-btn-sm"></a>
+                </div>
+            </li>
+            <li>
+                <dl class="game-count">
+                    <dt>00</dt>
+                    <dd>时</dd>
+                    <dt>00</dt>
+                    <dd>分</dd>
+                    <dt>00</dt>
+                    <dd>秒</dd>
+                </dl>
+                <div class="imgBox">
+                    <a href="http://www.lltao.com/m/73"><img src="../assets/img/game04.jpg" alt=""/></a>
+                </div>
+                <div class="tit">Nintendo 任天堂 3DS XL 掌上游戏机</div>
+                <div class="fd-col">
+                    <span class="money">参考价：1499.00元</span>
+                    <a href="http://www.lltao.com/m/73" class="lol-btn-sm"></a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
