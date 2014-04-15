0dd9954224315802159b1928fd89b8b524377b91
weelion <weelion@qq.com>
2014-04-15 21:08:22 +0800

    QQ客服在线状态优化

--- fuel/app/classes/classes/qqonline.php
+++ fuel/app/classes/classes/qqonline.php
@@ -2,35 +2,17 @@
 namespace Classes;
 
 class Qqonline {
-    private static $qq = '2698744419';
-    private static $cacheTime = 600;
   
-    public static function qqState($qq = '') {
-       if(empty($qq)) $qq = self::$qq;
-       $dir = APPPATH . 'tmp' . DS . 'qqstate' . DS;
-
-       if(!file_exists($dir)) {
-           mkdir($dir, 0755, true);
-       }
-       $stateTmp = $dir . $qq;
+    public static function qqState() {
+        $config = \Config::load('qq');
+        $qq = \Config::get('qq');
+        $stateTmp = APPPATH . 'tmp' . DS . 'qqstate' . DS . $qq;
        
-       $editTime = 0;
-       if(file_exists($stateTmp)) {
-           $editTime = filemtime($stateTmp);
-       }
+        $status = false;
+        if(file_exists($stateTmp)) {
+            $status = file_get_contents($stateTmp) == 1;
+        }
 
-       if(time() - $editTime > self::$cacheTime) {
-           $qIcon = file_get_contents('http://wpa.qq.com/pa?p=2:'.$qq.':52');
-           $len = strlen($qIcon);
-           if($len == 1627) {
-               file_put_contents($stateTmp, 0);
-               return false;
-           } else {
-               file_put_contents($stateTmp, 1);
-               return true;
-           }
-       } else {
-           return file_get_contents($stateTmp) == 1;
-       }
+        return $status;
     }
 }
