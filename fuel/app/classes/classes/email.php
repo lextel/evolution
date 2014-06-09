6efe2fa14c1f7f5ec6e1ffac654b9b508575a744
alenslan <lanchangqing322@gmail.com>
2014-06-09 19:45:38 +0800

    配置新的EMAIL发送模块

--- fuel/app/classes/classes/email.php
+++ fuel/app/classes/classes/email.php
@@ -10,7 +10,6 @@ use Fuel\core\Log;
 use Fuel\core\Config;
 
 use Email\Email as SysEmail;
-
 class Email {
     /*
     * 功能：最终邮件发送
@@ -19,12 +18,36 @@ class Email {
     * @param subject 发送邮件标题
     * @return boolean 发送成功 true, 发送失败 false
     */
-    public static function send($to, $view, $subject) {
+    public static function send($to, $view, $subject){
+        require 'email/PHPMailerAutoload.php';
+        Config::load('email');
+        $mail = new \PHPMailer();
+        $mail->isSMTP();
+        $mail->Debugoutput = 'html';
+        $mail->Host = Config::get('defaults.smtp.host');
+        $mail->Port = Config::get('defaults.smtp.port');
+        $mail->SMTPAuth = true;
+        $mail->Username = Config::get('defaults.smtp.username');
+        $mail->Password = Config::get('defaults.smtp.password');
+        $mail->setFrom(Config::get('defaults.from.email'), Config::get('defaults.from.name'));
+        $mail->addAddress($to);
+        $mail->Subject = $subject;
+        $mail->msgHTML($view);
+        //$mail->Body  = $view;
+        if (!$mail->send()){
+            Log::error("to:" . $to . "邮件发送错误: " . $mail->ErrorInfo);
+            return false;
+        }
+        return true;
+
+    }
+
+    public static function defaultSend($to, $view, $subject) {
         $email = SysEmail::forge();
         $email->to($to);
         $email->subject($subject);
         $email->html_body($view, true, false);
-        
+
         try
         {
            $email->send();
@@ -55,7 +78,7 @@ class Email {
     public static function checkemail($data) {
         $view = \View::forge($data['view'], $data);
         $res = self::send($data['email'], $view, $data['subject']);
-        return $res; 
+        return $res;
     }
     /*
     * 功能：检测邮箱的DOMAIN
