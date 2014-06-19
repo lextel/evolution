c3e9e4ad7c06a7b12c76ba6f2b16a3c43742eb59
alenslan <lanchangqing322@gmail.com>
2014-06-19 11:18:54 +0800

    快钱测试

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -5,8 +5,6 @@
 return array(
     'default' => array(
         'connection'  => array(
-            //'dsn'        => 'mysql:host=127.0.0.1;dbname=lltao',
-            //'dsn'        => 'mysql:host=192.168.3.10;dbname=llt_dev',
             'dsn'        => 'mysql:host=localhost;dbname=llt_bro',
             'username'   => 'root',
             'password'   => '@@__++///db',
