fd52b5410e1b7b6a445d2938dbbe40f220447339
alenslan <lanchangqing322@gmail.com>
2014-07-25 13:23:09 +0800

    修改底部的BRO路径以及去掉合作专区

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -127,11 +127,13 @@
                 <li>|</li>
                 <li><a href="<?php echo Uri::create('h/privacy');?>">隐私声明</a></li>
                 <li>|</li>
+                <?php if (0) { ?>
                 <li><a href="<?php echo Uri::create('h/cooperation');?>">合作专区</a></li>
+                <?php } ?>
                 <li>|</li>
                 <li class="lastest"><a href="<?php echo Uri::create('h/contact');?>">联系我们</a></li>
             </ul>
-            <P style="color:#5b5b5b">Copyright © 2014<?php echo date('Y') != 2014 ? '-'.date('Y') : '';?> <a href="http://www.lltaobro.com">www.lltaobro.com</a> 版权所有 <a href="http://www.miitbeian.gov.cn/" target="_blank" ref="nofollow">粤ICP备14017463号-1<!--服务器商要求加链接--></a></P>
+            <P style="color:#5b5b5b">Copyright © 2014<?php echo date('Y') != 2014 ? '-'.date('Y') : '';?> <a href="http://www.lltao.com">www.lltao.com</a> 版权所有 <a href="http://www.miitbeian.gov.cn/" target="_blank" ref="nofollow">粤ICP备14017463号-1<!--服务器商要求加链接--></a></P>
             <!--<div class="slogan"><img src=<?php echo Uri::create("/assets/images/slogan.png");?>></div>-->
             <div class="flink" style="text-align:center">
                 <span>友情链接:</span>
