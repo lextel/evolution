fcfc99a0d1bc1bb14f52df4823d4d148f0e00c91
alenslan <lanchangqing322@gmail.com>
2014-08-08 16:20:40 +0800

    增加友情连接页面1

---
+++ fuel/app/views/index/links.php
@@ -0,0 +1,56 @@
+<style type="text/css">
+        .links_list{
+            width: 980px;
+            margin: 0 auto;
+            background: #ffffff;
+            box-sizing: border-box;
+            border: 1px solid #eeeeee;
+        }
+        .links_list .tit{
+            height: 36px;
+            text-indent: 1em;
+            line-height: 36px;
+            font-size: 14px;
+            color: #000;
+            border-bottom: 2px solid #DD0404;
+        }
+        .links_list ul{
+            overflow: hidden;
+            min-height: 400px;
+            padding: 20px;
+        }
+        .links_list li{
+            float: left;
+            height: 38px;
+            width: 100%;
+            line-height: 38px;
+            border-bottom: 1px solid #EEEEEE;
+        }
+        .links_list li a{
+            display: block;
+            width: 167px;;
+            float: left;
+            height: 38px;
+            margin:0 10px;
+            overflow: hidden;
+            box-sizing: border-box;
+            font-size: 14px;
+        }
+        .links_list li a:hover{
+            text-decoration: underline;
+            color: #DD0404;
+        }
+</style>
+<div class="w links_list">
+        <h4 class="tit">友情链接</h4>
+        <ul>
+            <li><a href="http://bbs.anzhi.com" target="_blank">安智论坛</a>
+                <a href="http://www.xda.cn" target="_blank">XDA</a>
+                <a href="http://www.wanggouchao.com" target="_blank">网购潮</a>
+                <a href="http://www.kuaidi100.com/all/sf.shtml" target="_blank">顺丰快递查询</a>
+                <a href="http://www.yyyg.com" target="_blank">一元云购</a>             
+            </li>
+            <li><script type="text/javascript" src="http://links.webscan.360.cn/index/index/5a921b4ccb556a6f7b8b23b0ae066fe5"></script>
+            </li>
+        </ul>
+</div>
