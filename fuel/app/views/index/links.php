936b6845f42bf4dd526fa81d58cc15b13ffb0151
alenslan <lanchangqing322@gmail.com>
2014-08-26 11:20:05 +0800

    添加友情连接-集分宝

--- fuel/app/views/index/links.php
+++ fuel/app/views/index/links.php
@@ -69,6 +69,7 @@
                 <a href="http://www.vip.com" target="_blank">唯品会</a>
                 <a href="http://www.jd.com" target="_blank">京东</a>
                 <a href="http://www.1yyg.com" target="_blank">一元云购</a>
+                <a href="http://www.51jfb.net" target="_blank">集分宝</a>
             </li>
         </ul>
         <h4 class="tit">申请步骤：</h4>
